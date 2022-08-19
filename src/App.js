c5ef956b608bb5324519ec1ad5fa03ec2e41bf31
Akshar Mehta <aksharmehta@Akshars-MacBook-Air.local>
2022-08-20 01:31:11 +0530

    Added functionalities, little UI/UX to do

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer";
+// import backgroundImage from "./assets/background.jpeg";
+import Header from "./components/Header";
+import axios from "axios";
+import Home from "./Pages/Home";
+import Quiz from "./Pages/Quiz";
+import Result from "./Pages/Result";
 function App() {
+  const [name, setName] = useState("");
+  const [questions, setQuestions] = useState();
+  const [score, setScore] = useState(0);
+  const fetchQuestions = async (category = "", difficulty = "") => {
+    const { data } = await axios.get(
+      `https://opentdb.com/api.php?amount=10${
+        category && `&category=${category}`
+      }${difficulty && `&difficulty=${difficulty}`}&type=multiple`
+    );
+    // console.log(data);
+    setQuestions(data.results);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div
+        className="app"
+        style={{ backgroundImage: "url(./background.jpeg)" }}
+      >
+        <Header />
+        <Routes>
+          <Route
+            path="/quiz"
+            element={
+              <Quiz
+                name={name}
+                questions={questions}
+                score={score}
+                setScore={setScore}
+                setQuestions={setQuestions}
+              />
+            }
+          />
+          <Route
+            path="/result"
+            element={<Result name={name} score={score} />}
+          />
+          <Route
+            path="/"
+            exact
+            element={
+              <Home
+                name={name}
+                setName={setName}
+                fetchQuestions={fetchQuestions}
+              />
+            }
+          />
+        </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
